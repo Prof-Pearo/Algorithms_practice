9f1cca154325cf0171f5f9f9a9219ae39145ba09
Prof-Pearo <tiproffesor@gmail.com>
2026-05-09 21:07:04 +0600

    Add files via upload

---
+++ Math/Factorial.py
@@ -0,0 +1,14 @@
+def factorial(n: int) -> int:
+    if n < 0:
+        raise ValueError("Negative numbers are not allowed")
+
+    result = 1
+
+    for i in range(2, n + 1):
+        result *= i
+
+    return result
+
+
+if __name__ == "__main__":
+    print(factorial(5))
